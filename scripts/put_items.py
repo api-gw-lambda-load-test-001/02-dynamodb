d8c9d6bda193852c997296d220a04f771ad62c5a
sinofseven <em.s.00001@gmail.com>
2020-12-23 11:08:21 +0900

    impl prepare dynamodb

--- scripts/put_items.py
+++ scripts/put_items.py
@@ -18,9 +18,20 @@ def create_md5_hexdigest(num: int) -> str:
     return hashlib.md5(raw).hexdigest()
 
 
+def create_batch_writer(table_name: str):
+    resource = boto3.resource("dynamodb")
+    table = resource.Table(table_name)
+    return table.batch_writer()
+
+
 def main():
     table_name = get_dynamodb_table_name()
-    print(table_name)
+    with create_batch_writer(table_name) as batch:
+        for i in range(110000):
+            item = {
+                'id': create_md5_hexdigest(i + 1)
+            }
+            batch.put_item(item)
 
 
 if __name__ == '__main__':
